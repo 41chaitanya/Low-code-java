4f94159bf04a1fae816ddeedde0f22ecf72a03cd
41chaitanya <chaitanya4141sharma@gmail.com>
2026-02-15 12:04:02 +0530

    INIT:project init , CONFIG:postgres  configuration locally pSQL

---
+++ simulator_backend/src/main/java/com/codecreator/simulator/JavaCodeSimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.codecreator.simulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCodeSimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCodeSimulatorApplication.class, args);
+	}
+
+}
---
+++ simulator_backend/src/test/java/com/codecreator/simulator/JavaCodeSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecreator.simulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCodeSimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
